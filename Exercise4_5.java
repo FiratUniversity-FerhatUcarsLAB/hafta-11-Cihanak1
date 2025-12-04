ad0f0f973019dfca792f52e43a0f70b1573ca2d5
Cihanak1 <250541107@firat.edu.tr>
2025-12-04 18:14:37 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -1,27 +1,58 @@
-public class Exercise4_5 {
+Ad Soyad: Cihan Akalın 
+Ogrenci No: 250541107 
+Odev No: 4_5
 
-    public static void zoop(String fred, int bob) {
-        System.out.println(fred);
-        if (bob == 5) {
-            ping("not ");
-        } else {
-            System.out.println("!");
-        }
-    }
+Soruların Cevapları:
+1. zoop metodu ikinci kez çağrıldığında programın durumunu gösteren Stack Diyagramı
 
-    public static void main(String[] args) {
-        int bizz = 5;
-        int buzz = 2;
-        zoop("just for", bizz);
-        clink(2 * buzz);
-    }
+1) main başlar (Satır 10). bizz = 5, buzz = 2.
 
-    public static void clink(int fork) {
-        System.out.print("It's ");
-        zoop("breakfast ", fork);
-    }
+2) zoop("just for", 5) çağrılır (Satır 13). (1. Çağrı)
 
-    public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more ");
-    }
-}
+3) zoop çalışır, ping'i çağırır (Satır 4).
+
+4) ping biter, zoop biter. Kontrol main'e döner (Satır 14).
+
+5) clink(4) çağrılır (Satır 14). (2 * buzz = 4)
+
+6) clink çalışır, "It's " yazdırılır (Satır 18).
+
+7) zoop("breakfast ", 4) çağrılır (Satır 19). (2. Çağrı)
+
+
+
+
+
+Konum	                       Metot Adı	                Parametreler	                   Yerel Değişkenler	           Geri Dönüş Adresi (Sonraki Satır)
+En Üst (Aktif)	                zoop	                    fred = "breakfast ", bob = 4	     Yok	                               Satır 2
+2.	                            clink	                    fork = 4	                         Yok	                               Satır 20
+En Alt (Başlangıç)	            main	                    args (String[])	                     bizz = 5, buzz = 2	                   Satır 15
+
+
+Satır	                          Metot	                          İşlem	                                      Çıktı	                       Açıklama
+13	                              main	                          zoop("just for", 5) çağrısı		
+2	                              zoop (1)	                      println("just for")	                      just for\n	               println satır sonu ekler.
+3	                              zoop (1)	                      if (5 == 5) -> True		
+4	                              zoop (1)	                      ping("not ") çağrısı		
+23	                              ping	                          println("any " + "not " + "more ")          any not more \n	
+14	                              main	                          clink(4) çağrısı		
+18	                              clink	                          print("It's ")	                          It's	                       print satır sonu eklemez.
+19	                              clink	                          zoop("breakfast ", 4) çağrısı		
+2	                              zoop (2)	                      println("breakfast ")	                      breakfast \n	
+3	                              zoop (2)	                      if (4 == 5) -> False		
+6	                              zoop (2)	                      println("!")	                              !\n	
+15	                              main	main sonu
+
+
+
+Programın ürettiği çıktı:
+    Sırasıyla;
+"just for
+any not more 
+It's breakfast 
+!"
+
+
+
+
+    
